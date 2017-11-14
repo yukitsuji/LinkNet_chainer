4f0c2113ce7b812216f72054b9bd2fca46338619
Yuki Tsuji <yukitsuji020832@gmail.com>
2017-11-14 17:37:01 +0900

    Complete model

--- linknet/models/linknet_paper.py
+++ linknet/models/linknet_paper.py
@@ -28,12 +28,13 @@ def _without_cudnn(f, x):
 class Conv(chainer.Chain):
     "Convolution2D for inference module"
     def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
-                 nobias=False, upsample=False):
+                 nobias=False, upsample=False, outsize=None):
         super(Conv, self).__init__()
         with self.init_scope():
             if upsample:
                 self.conv = L.Deconvolution2D(
-                                in_ch, out_ch, ksize, stride, pad, nobias=nobias)
+                                in_ch, out_ch, ksize, stride, pad,
+                                nobias=nobias, outsize=outsize)
             else:
                 if dilation > 1:
                     self.conv = L.DilatedConvolution2D(
@@ -52,9 +53,9 @@ class Conv(chainer.Chain):
 class ConvBN(Conv):
     """Convolution2D + Batch Normalization"""
     def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
-                 nobias=False, upsample=False):
+                 nobias=False, upsample=False, outsize=None):
         super(ConvBN, self).__init__(in_ch, out_ch, ksize, stride, pad,
-                                     dilation, nobias, upsample)
+                                     dilation, nobias, upsample, outsize)
 
         self.add_link("bn", L.BatchNormalization(out_ch, eps=1e-5, decay=0.95))
 
@@ -64,13 +65,41 @@ class ConvBN(Conv):
     def predict(self, x):
         return self.bn(self.conv(x))
 
+class ConvReLU(Conv):
+    """Convolution2D + ReLU"""
+    def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
+                 nobias=False, upsample=False, outsize=None):
+        super(ConvReLU, self).__init__(in_ch, out_ch, ksize, stride, pad,
+                                        dilation, nobias, upsample, outsize)
+
+    def __call__(self, x):
+        return F.relu(self.conv(x))
+
+    def predict(self, x):
+        return F.relu(self.conv(x))
+
+
+class ConvBNReLU(ConvBN):
+    """Convolution2D + Batch Normalization + ReLU"""
+    def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
+                 nobias=False, upsample=False, outsize=None):
+        super(ConvBNReLU, self).__init__(in_ch, out_ch, ksize, stride, pad,
+                                          dilation, nobias, upsample, outsize)
+
+
+    def __call__(self, x):
+        return F.relu(self.bn(self.conv(x)))
+
+    def predict(self, x):
+        return F.relu(self.bn(self.conv(x)))
+
 
 class ConvPReLU(Conv):
     """Convolution2D + PReLU"""
     def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
-                 nobias=False, upsample=False):
+                 nobias=False, upsample=False, outsize=None):
         super(ConvPReLU, self).__init__(in_ch, out_ch, ksize, stride, pad,
-                                        dilation, nobias, upsample)
+                                        dilation, nobias, upsample, outsize)
 
         self.add_link("prelu", L.PReLU())
 
@@ -84,9 +113,9 @@ class ConvPReLU(Conv):
 class ConvBNPReLU(ConvBN):
     """Convolution2D + Batch Normalization + PReLU"""
     def __init__(self, in_ch, out_ch, ksize, stride=1, pad=1, dilation=1,
-                 nobias=False, upsample=False):
+                 nobias=False, upsample=False, outsize=None):
         super(ConvBNPReLU, self).__init__(in_ch, out_ch, ksize, stride, pad,
-                                          dilation, nobias, upsample)
+                                          dilation, nobias, upsample, outsize)
 
         self.add_link("prelu", L.PReLU())
 
@@ -143,11 +172,10 @@ class InitialBlock(chainer.Chain):
             this_mod = sys.modules[__name__]
             conv_type = "ConvBN" if use_bn else "Conv"
             activation = "PReLU" if use_prelu else "ReLU"
-            conv_type2 = conv_type + activation
-            conv_type2 = "Symmetric" + conv_type2 if symmetric else conv_type2
-            ConvBlock = getattr(this_mod, conv_type2)
+            conv_type = conv_type + activation
+            ConvBlock = getattr(this_mod, conv_type)
             self.conv = ConvBlock(in_ch, out_ch, ksize, stride,
-                                  pad, nobias=nobias, upsample=upsample)
+                                  pad, nobias=nobias)
         self.psize = psize
         self.ppad = int((psize - 1) / 2)
 
@@ -160,134 +188,104 @@ class InitialBlock(chainer.Chain):
         return F.max_pooling_2d(x, self.psize, 2, self.ppad)
 
 
-class Block(chainer.Chain):
-    """Block Abstract"""
-    def __init__(self, in_ch=3, mid_ch=0, out_ch=13, ksize=3, stride=1, pad=1,
-                 dilation=1, nobias=False, symmetric=False, drop_ratio=0.0,
-                 downsample=False, upsample=False, p=None, use_bn=True):
-        super(Block, self).__init__()
-        k1, k2, s1 = self.calc_param(downsample, symmetric, upsample)
-        self.p = p
-        self.drop_ratio = drop_ratio
+class ResBacisBlock(chainer.Chain):
+    """Basic block of ResNet18 and ResNet34"""
+    def __init__(self, in_ch=3, out_ch=13, downsample=False, use_bn=True):
+        super(ResBacisBlock, self).__init__()
         self.downsample = downsample
-        self.upsample = upsample
-
         with self.init_scope():
             this_mod = sys.modules[__name__]
             conv_type = "ConvBN" if use_bn else "Conv"
-            ConvBlock = getattr(this_mod, conv_type + "PReLU")
-            self.conv1 = ConvBlock(in_ch, mid_ch, k1, s1, 0, nobias=True)
+            ConvBlock = getattr(this_mod, conv_type + "ReLU")
+            self.conv1 = ConvBlock(in_ch, out_ch, 3, 1, 1, nobias=True)
+            ConvBlock = getattr(this_mod, conv_type)
+            self.conv2 = ConvBlock(in_ch, out_ch, 3, 1, 1, nobias=True)
+            if self.downsample:
+                self.conv3 = ConvBlock(in_ch, out_ch, 1, 2, 0, nobias=True)
 
-            conv_type2 = conv_type + "PReLU"
-            conv_type2 = "Symmetric" + conv_type2 if symmetric else conv_type2
+    def __call__(self, x):
+        h1 = self.covn2(self.conv(x))
+        if self.downsample:
+            return F.relu(h1 + self.conv3(x))
+        else:
+            return F.relu(h1 + x)
+
+
+class ResBlock18(chainer.Chain):
+    """Initial Block"""
+    def __init__(self, use_bn=True, train=True):
+        super(ResBlock18, self).__init__()
+        self.train = train
+        with self.init_scope():
+            self.block1_1 = ResBacisBlock(64, 64, use_bn=use_bn)
+            self.block1_2 = ResBacisBlock(64, 64, use_bn=use_bn)
+            self.block2_1 = ResBacisBlock(64, 128, use_bn=use_bn,
+                                          downsample=True)
+            self.block2_2 = ResBacisBlock(128, 128, use_bn=use_bn)
+            self.block3_1 = ResBacisBlock(128, 256, use_bn=use_bn,
+                                          downsample=True)
+            self.block3_2 = ResBacisBlock(256, 256, use_bn=use_bn)
+            self.block4_1 = ResBacisBlock(256, 512, use_bn=use_bn,
+                                          downsample=True)
+            self.block4_2 = ResBacisBlock(512, 512, use_bn=use_bn)
+
+    def pytorch2chainer(path):
+        pass
+
+    def __call__(self, x):
+        with chainer.using_config('train', self.train):
+            h1 = self.block1_2(self.block1_1(x))
+            h2 = self.block2_2(self.block2_1(h1))
+            h3 = self.block3_2(self.block3_1(h2))
+            h4 = self.block4_2(self.block4_1(h3))
+            return h1, h2, h3, h4
+
+    def predict(self, x):
+        x = self.block1_2(self.block1_1(x))
+        x = self.block2_2(self.block2_1(x))
+        x = self.block3_2(self.block3_1(x))
+        return self.block4_2(self.block4_1(x))
+
+
+class DecoderBlock(chainer.Chain):
+    """DecoderBlock Abstract"""
+    def __init__(self, in_ch=3, mid_ch=0, out_ch=13, ksize=3, stride=1, pad=1,
+                 residual=False, nobias=False, outsize=None,
+                 upsample=False, p=None, use_bn=True, use_prelu=False):
+        super(DecoderBlock, self).__init__()
+        self.residual = residual
+        with self.init_scope():
+            this_mod = sys.modules[__name__]
+            conv_type = "ConvBN" if use_bn else "Conv"
+            activation = "PReLU" if use_prelu else "ReLU"
+            ConvBlock = getattr(this_mod, conv_type + activation)
+            self.conv1 = ConvBlock(in_ch, mid_ch, 1, 1, 0, nobias=True)
+
+            conv_type2 = conv_type + activation
             ConvBlock = getattr(this_mod, conv_type2)
-            self.conv2 = ConvBlock(mid_ch, mid_ch, k2, stride,
-                                   pad, dilation,
+            self.conv2 = ConvBlock(mid_ch, mid_ch, ksize, stride, pad,
                                    nobias=False,
-                                   upsample=upsample)
+                                   upsample=upsample,
+                                   outsize=outsize)
 
             ConvBlock = getattr(this_mod, conv_type)
             self.conv3 = ConvBlock(mid_ch, out_ch, 1, 1, 0, nobias=True)
-            self.prelu = L.PReLU()
-            if downsample:
-                ConvBlock = getattr(this_mod, conv_type)
-                self.conv = ConvBlock(in_ch, out_ch, 1, 1, 0, nobias=True)
-                self.p = F.MaxPooling2D(2, 2)
-            if upsample:
-                ConvBlock = getattr(this_mod, conv_type)
-                self.conv = ConvBlock(in_ch, out_ch, 1, 1, 0, nobias=True)
-
-    def calc_param(self, downsample, symmetric, upsample):
-        k1, s1 = (2, 2) if downsample else (1, 1)
-        k2 = 2 if upsample else (5 if symmetric else 3)
-        return k1, k2, s1
-
-    def _upsampling_2d(self, x, pool):
-        if x.shape != pool.indexes.shape:
-            min_h = min(x.shape[2], pool.indexes.shape[2])
-            min_w = min(x.shape[3], pool.indexes.shape[3])
-            x = x[:, :, :min_h, :min_w]
-            pool.indexes = pool.indexes[:, :, :min_h, :min_w]
-        outsize = (x.shape[2] * 2, x.shape[3] * 2)
-        return F.upsampling_2d(
-            x, pool.indexes, ksize=(pool.kh, pool.kw),
-            stride=(pool.sy, pool.sx), pad=(pool.ph, pool.pw), outsize=outsize)
 
     def __call__(self, x):
         h1 = self.conv1(x)
         h1 = self.conv2(h1)
         h1 = self.conv3(h1)
-        h1 = spatial_dropout(h1, self.drop_ratio)
-        if self.downsample:
-            h1 += self.conv(_without_cudnn(self.p, x))
-        elif self.upsample:
-            h1 += self._upsampling_2d(self.conv(x), self.p)
-        else:
-            h1 += x
-        # h1 = h1 if not self.downsample else h1 + self.bn(self.conv(x))
-        return self.prelu(h1)
+        if self.residual:
+            return F.relu(h1 + x)
+        return F.relu(h1)
 
     def predict(self, x):
         h1 = self.conv1(x)
         h1 = self.conv2(h1)
         h1 = self.conv3(h1)
-        if self.downsample:
-            h1 += self.conv(_without_cudnn(self.p, x))
-        elif self.upsample:
-            h1 += self._upsampling_2d(self.conv(x), self.p)
-        else:
-            h1 += x
-        return self.prelu(h1)
-
-
-class Bottleneck2(chainer.Chain):
-    """Bottleneck1"""
-    def __init__(self, in_ch=3, mid_ch=0, out_ch=13, drop_ratio=0.0,
-                 use_bn=True):
-        super(Bottleneck2, self).__init__()
-        basic_config = {"in_ch": in_ch, "mid_ch": mid_ch, "out_ch": out_ch,
-                        "drop_ratio": drop_ratio, "use_bn": use_bn}
-        with self.init_scope():
-            self.block1 = Block(**basic_config)
-            self.block2 = Block(**self.to_dilated(basic_config, 2))
-            self.block3 = Block(**self.to_symmetric(basic_config))
-            self.block4 = Block(**self.to_dilated(basic_config, 4))
-            self.block5 = Block(**basic_config)
-            self.block6 = Block(**self.to_dilated(basic_config, 8))
-            self.block7 = Block(**self.to_symmetric(basic_config))
-            self.block8 = Block(**self.to_dilated(basic_config, 16))
-
-    def to_dilated(self, basic_config, dilation):
-        config = basic_config.copy()
-        config.update({"dilation": dilation, "pad": dilation})
-        return config
-
-    def to_symmetric(self, basic_config):
-        config = basic_config.copy()
-        config.update({"symmetric": True})
-        return config
-
-    def __call__(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = self.block7(x)
-        x = self.block8(x)
-        return x
-
-    def predict(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = self.block7(x)
-        x = self.block8(x)
-        return x
+        if self.residual:
+            return F.relu(h1 + x)
+        return F.relu(h1)
 
 
 class FullConv(chainer.Chain):
@@ -306,38 +304,68 @@ class FullConv(chainer.Chain):
 
 class LinkNetBasic(chainer.Chain):
     """LinkNet Basic for semantic segmentation."""
-    def __init__(self, model_config, pretrained_model=None):
+    def __init__(self, config, pretrained_model=None):
         super(LinkNetBasic, self).__init__()
         n_class = None
         this_mod = sys.modules[__name__]
-        self.layers = []
         pretrained_path = parse_dict(pretrained_model, 'path', None)
-        pretrained_name = parse_dict(pretrained_name, 'name', None)
+        size = parse_dict(config, 'size', (512, 1024))
         with self.init_scope():
-            for key, config in model_config.items():
-                Model = getattr(this_mod, config['type'])
-                loop = parse_dict(config, 'loop', 1)
-                for l in range(loop):
-                    layer = Model(**config['args'])
-                    if hasattr(layer, "upsample") and layer.upsample:
-                        layer.p = self.get_pool_index(config['p'])
-                    name = key + '_{}'.format(l)
-                    setattr(self, name, layer)
-                    self.layers.append(getattr(self, name))
-                if key == pretrained_name:
-                    chainer.serializers.load_npz(pretrained_path, self)
+            BlockType = getattr(this_mod, config['initial_block']['type'])
+            self.initial_block = BlockType(**config['initial_block']['args'])
 
-        if  pretrained_path and pretrained_name is None:
+            BlockType = getattr(this_mod, config['resblock']['type'])
+            self.resblock = BlockType(**config['resblock']['args'])
+
+            BlockType = getattr(this_mod, config['decoder_block4']['type'])
+            args = self.parse_outsize(config, 'decoder_block4')
+            self.decoder4 = BlockType(**args)
+
+            BlockType = getattr(this_mod, config['decoder_block3']['type'])
+            args = self.parse_outsize(config, 'decoder_block3')
+            self.decoder3 = BlockType(**args)
+
+            BlockType = getattr(this_mod, config['decoder_block2']['type'])
+            args = self.parse_outsize(config, 'decoder_block2')
+            self.decoder2 = BlockType(**args)
+
+            BlockType = getattr(this_mod, config['decoder_block1']['type'])
+            args = self.parse_outsize(config, 'decoder_block1')
+            self.decoder1 = BlockType(**args)
+
+            BlockType = getattr(this_mod, config['finalblock1']['type'])
+            self.finalblock1 = BlockType(**config['finalblock1']['args'])
+
+            BlockType = getattr(this_mod, config['finalblock2']['type'])
+            self.finalblock2 = BlockType(**config['finalblock2']['args'])
+
+            BlockType = getattr(this_mod, config['finalblock3']['type'])
+            self.finalblock3 = BlockType(**config['finalblock3']['args'])
+
+        if pretrained_path:
             chainer.serializers.load_npz(pretrained_path, self)
 
-    def get_pool_index(self, p_name):
-        for layer in self.children():
-            if p_name in layer.name:
-                return layer.p
+    def parse_outsize(self, config, key):
+        args = config[key]['args']
+        scale = config[key]['scale']
+        args['outsize'] = (size[0]/scale, size[1]/scale)
+        return args
+
 
     def __call__(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        x = self.initial_block(x)
+        h1, h2, h3, h4 = self.resblock(x)
+        x = self.decoder4(h4)
+        x += h3
+        x = self.decoder3(x)
+        x += h2
+        x = self.decoder2(x)
+        x += h1
+        x = self.decoder1(x)
+
+        x = self.finalblock1(x)
+        x = self.finalblock2(x)
+        x = self.finalblock3(x)
         return x
 
     def predict(self, x):
@@ -346,19 +374,19 @@ class LinkNetBasic(chainer.Chain):
             x = self.xp.asarray(x)
             if x.ndim == 3:
                 x = self.xp.expand_dims(x, 0)
-            for layer in self.layers:
-                x = layer.predict(x)
+            x = self.initial_block(x)
+            h1, h2, h3, h4 = self.resblock(x)
+            x = self.decoder4(h4)
+            x += h3
+            x = self.decoder3(x)
+            x += h2
+            x = self.decoder2(x)
+            x += h1
+            x = self.decoder1(x)
+
+            x = self.finalblock1(x)
+            x = self.finalblock2(x)
+            x = self.finalblock3(x)
             label = self.xp.argmax(x.data, axis=1).astype("i")
             label = chainer.cuda.to_cpu(label)
             return list(label)
-
-    #     h = self._upsampling_2d(h, p4)
-    #     h = self.conv_decode4_bn(self.conv_decode4(h))
-    #     h = self._upsampling_2d(h, p3)
-    #     h = self.conv_decode3_bn(self.conv_decode3(h))
-    #     h = self._upsampling_2d(h, p2)
-    #     h = self.conv_decode2_bn(self.conv_decode2(h))
-    #     h = self._upsampling_2d(h, p1)
-    #     h = self.conv_decode1_bn(self.conv_decode1(h))
-    #     score = self.conv_classifier(h)
-    #     return score
